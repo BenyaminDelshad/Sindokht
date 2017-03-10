54a9cd3e67434267cd6ee65499f27ffe8ca0f12a
benyamindelshad <benyamindelshad@gmail.com>
2017-03-10 14:14:59 +0330

    Complete Tokenizer! B-)

--- Codes/Tokenizer.py
+++ Codes/Tokenizer.py
@@ -3,19 +3,13 @@
 from __future__ import unicode_literals
 from hazm import *
 
-# /home/benyamin/Desktop/Sindokht/Docs/test.txt
+# ../Docs/test.txt
+
 
 def process(filename):
-    filename = '../Docs/test.txt'
     tmp = open(filename, 'r').read()
     s = unicode(tmp, "utf-8")
-    print type(s)
-    print 'text:\n'
-    print s
     normalizer = Normalizer()
-    normalizer.normalize(s)
-    print s
-    print word_tokenize(s)
+    s = normalizer.normalize(s)
+    return word_tokenize(s)
 
-
-process('a')
